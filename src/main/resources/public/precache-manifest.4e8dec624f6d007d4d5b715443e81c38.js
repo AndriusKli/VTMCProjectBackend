ef650d41a5b2c6dd96311ac395a09c932910e5be
AndriusKli <andriuskliunka@gmail.com>
2020-05-17 22:59:03 +0300

    Added latest updates.

---
+++ src/main/resources/public/precache-manifest.4e8dec624f6d007d4d5b715443e81c38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80a22801ea6cf5bce88d2a86946f4bee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "027ce46f43dbdf025672",
+    "url": "/static/css/main.3751746d.chunk.css"
+  },
+  {
+    "revision": "f6e1119c3f6227ff7918",
+    "url": "/static/js/2.8f91d177.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f91d177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027ce46f43dbdf025672",
+    "url": "/static/js/main.f247d9c1.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  },
+  {
+    "revision": "035ba094b80cd1e455e17368cabf3db6",
+    "url": "/static/media/titulinis.035ba094.png"
+  }
+]);
