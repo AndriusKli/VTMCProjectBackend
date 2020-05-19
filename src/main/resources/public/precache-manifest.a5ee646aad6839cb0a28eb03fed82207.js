b65a8df9389ba536a48be2ec6758dcb07a9212a0
AndriusKli <andriuskliunka@gmail.com>
2020-05-19 19:14:15 +0300

    Implemented a fix to frontend.

---
+++ src/main/resources/public/precache-manifest.a5ee646aad6839cb0a28eb03fed82207.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d74677d56a24e45cdf02101c08bec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca8f5b0fe86c5352074",
+    "url": "/static/css/main.3751746d.chunk.css"
+  },
+  {
+    "revision": "f6e1119c3f6227ff7918",
+    "url": "/static/js/2.8f91d177.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8f91d177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca8f5b0fe86c5352074",
+    "url": "/static/js/main.c92cf654.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  },
+  {
+    "revision": "035ba094b80cd1e455e17368cabf3db6",
+    "url": "/static/media/titulinis.035ba094.png"
+  }
+]);
