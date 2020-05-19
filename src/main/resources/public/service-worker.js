3030553747046637cd4b06ab93e43ee56bb2bd67
AndriusKli <andriuskliunka@gmail.com>
2020-05-19 19:06:01 +0300

    Implemented a fix to frontend.

--- src/main/resources/public/service-worker.js
+++ src/main/resources/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e8dec624f6d007d4d5b715443e81c38.js"
+  "./precache-manifest.2add35d6a3c6211b3caaea9e9e9e4682.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
