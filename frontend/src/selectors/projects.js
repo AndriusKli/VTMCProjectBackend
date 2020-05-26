ff814d54d1be0cdb4af4e52be5b47d3206b36d21
AndriusKli <andriuskliunka@gmail.com>
2020-05-26 15:40:42 +0300

    Fixing a mess I made

--- frontend/src/selectors/projects.js
+++
@@ -1,8 +0,0 @@
-export default (projects, { searchBy, filterStatusBy }) => {
-    return projects.filter((project) => {
-        const searchByMatch = project.projectName.toLowerCase().includes(searchBy.toLowerCase());
-        const filterStatusByMatch = project.projectStatus.includes(filterStatusBy);
-        
-        return searchByMatch && filterStatusByMatch;
-    });
-}
--- frontend/src/selectors/tasks.js
+++
@@ -1,8 +0,0 @@
-export default (tasks, { searchBy, filterStatusBy }) => {
-    return tasks.filter((task) => {
-        const searchByMatch = task.taskName.toLowerCase().includes(searchBy.toLowerCase());
-        const filterStatusByMatch = task.taskStatus.includes(filterStatusBy);
-        
-        return searchByMatch && filterStatusByMatch;
-    });
-}
